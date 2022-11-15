3e3f0e1d6eaa23517e38d0e0c331ab3cece61420
r.yoshimoto <r>
2022-11-15 19:08:39 +0900

    toggleClass から slideClass を使用

--- js/jquery.js
+++ js/jquery.js
@@ -1,5 +1,6 @@
 $(function(){
     $('.menu-btn').on('click', function(){
-      $('.menu').toggleClass('is-active');
+      //$('.menu').toggleClass('is-active');
+      $('.menu').slideToggle('1000');	
     });
   }());
