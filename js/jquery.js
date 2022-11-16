08c7f655b1fc6dddd5a2cce6b1f799f7667d0aa4
kazuto sato <private10969@yahoo.co.jp>
2022-11-16 16:26:13 +0900

    no message

--- js/jquery.js
+++ js/jquery.js
@@ -1,5 +1,43 @@
 $(function(){
-    $('.menu-btn').on('click', function(){
-      $('.menu').toggleClass('is-active');
-    });
-  }());
+    $('button').on('click',function(){
+        $('.menu').slideToggle();
+    })
+
+
+
+
+	$('a[href^="#"]').on('click',function(){
+		var speed = 500;
+		var href= $(this).attr("href");
+		var target = $(href == "#" || href == "" ? 'html' : href);
+		var position = target.offset().top;
+		$("html, body").animate({scrollTop:position}, speed, "swing");
+		return false;
+	});
+
+	$('a[href^="#"]').on('click',function(){
+		var speed = 500;
+		var href= $(this).attr("href");
+		var target = $(href == "#" || href == "" ? 'html' : href);
+		var position = target.offset().top;
+		$("html, body").animate({scrollTop:position}, speed, "swing");
+		return false;
+	});
+	
+	$('.slide-items').slick({
+		autoplay: true, // スライド自動再生
+		slidesToShow: 2, // 表示させるスライドの枚数
+		responsive: [{ // レスポンシブ設定
+		  breakpoint: 768, // ブラウザ幅768px以下
+		  settings: {
+			slidesToShow: 1,
+		  }
+		}]
+	  });
+	 
+	$('.slider').slick();
+	  
+	
+
+
+});
