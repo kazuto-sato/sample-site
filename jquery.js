e1b91538a45df94391fb61bcb85fd9a01c63319c
kazuto sato <private10969@yahoo.co.jp>
2022-11-16 14:35:53 +0900

    no message

--- jquery.js
+++ jquery.js
@@ -1,6 +1,29 @@
 $(function(){
-    $('.menu-btn').on('click', function(){
-      $('.menu').slideToggle('1000');
-    });
-  }());
+    $('buttom').on('click',function(){
+        $('.menu').slideToggle();
+    })
+})
 
+
+
+$(function(){
+	$('a[href^="#"]').on('click',function(){
+		var speed = 500;
+		var href= $(this).attr("href");
+		var target = $(href == "#" || href == "" ? 'html' : href);
+		var position = target.offset().top;
+		$("html, body").animate({scrollTop:position}, speed, "swing");
+		return false;
+	});
+});
+
+$(function(){
+	$('a[href^="#"]').on('click',function(){
+		var speed = 500;
+		var href= $(this).attr("href");
+		var target = $(href == "#" || href == "" ? 'html' : href);
+		var position = target.offset().top;
+		$("html, body").animate({scrollTop:position}, speed, "swing");
+		return false;
+	});
+});
