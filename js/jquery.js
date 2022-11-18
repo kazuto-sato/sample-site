9e3ff972bbf9c2d539687c750b61be8903de4f0a
kazuto sato <private10969@yahoo.co.jp>
2022-11-18 13:05:21 +0900

    no message

--- js/jquery.js
+++ js/jquery.js
@@ -15,18 +15,9 @@ $(function(){
 		return false;
 	});
 
-	$('a[href^="#"]').on('click',function(){
-		var speed = 500;
-		var href= $(this).attr("href");
-		var target = $(href == "#" || href == "" ? 'html' : href);
-		var position = target.offset().top;
-		$("html, body").animate({scrollTop:position}, speed, "swing");
-		return false;
-	});
-	
 	$('.slide-items').slick({
 		autoplay: true, // スライド自動再生
-		slidesToShow: 2, // 表示させるスライドの枚数
+		slidesToShow: 1, // 表示させるスライドの枚数
 		responsive: [{ // レスポンシブ設定
 		  breakpoint: 768, // ブラウザ幅768px以下
 		  settings: {
@@ -34,10 +25,47 @@ $(function(){
 		  }
 		}]
 	  });
-	 
-	$('.slider').slick();
-	  
+	
+	
+	const now = new Date();
+	const date =now.getDate(); 
+		console.log(date);
+
+	/*
+	const x = document.getElementsByTagName("td");
+	  console.log(x);
+	
+	$("td").each(function(i, o){
+		console.log('インデックス番号:' + i + '、テキスト:' + $(o).text());
+	});
+	*/
+
+	const x = ; 
+
+	if (date == x){
+		console.log('AAAAAAAAAAAAAAA');
+	}
+
+	
+	
+	/*
+	console.log(getElementById('#td');
+
 	
 
 
+	/*
+	const date = document. querySelector(td);
+	  console.log('tdだぞ'); */
+
+	/*const = this.documentElement.querySelector('id');*/
+	
+	/*
+	$('td').each(function(index){
+		console.log(index);
+	})
+	
+	if　(now == ){
+		$('追加したいタグ').addClass('is-active');
+	};*/
 });
