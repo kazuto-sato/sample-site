9359a5faeecf752436348f41d04f005669736efb
r.yoshimoto <r>
2022-11-22 19:26:49 +0900

    no message

--- Bingo/js/script.js
+++ Bingo/js/script.js
@@ -4,31 +4,45 @@ $(function(){
     for (let a=1;a<=75;a++){
         array.push(a);
     }
-    console.log(array);
 
     /*配列からランダムに要素を取り出す*/
     let rndnum = array[Math.floor(Math.random()*array.length)];
-    console.log(rndnum);
 
-    
-   
-   
-    const selected = randomSelect(array.slice(), 25);
+
+    $(document).on('click','.create_card',function(i,o) {
+    	randomSelect(array.slice(), 25);
+    });
  
     // 配列arrayからランダムにnum個の要素を取り出す
     function randomSelect(array, num){
         let newArray = [];
+        
+        let html ='';
+        html += '<tr>'
         while(newArray.length < num && array.length > 0){
-            // 配列からランダムな要素を選ぶ
-            const rand = Math.floor(Math.random() * array.length);
-            // 選んだ要素を別の配列に登並べてく
+        	if(array.length % 5 == 0) {
+        		html += '<tr>'	
+        	}
+        	const rand = Math.floor(Math.random() * array.length);
+        	html += '<td class="num-'+ array[rand] +'">' + array[rand] + '</td>';
             newArray.push(array[rand]);
-            //取り出した値は選ばない→もとの配列からは削除する    
             array.splice(rand, 1);
+            if(array.length % 5 == 0) {
+        		html += '</tr>'	
+        	}
         }
+        console.log(html);
+        $('.container table').append(html);
         console.log(newArray)
     };
-    
-   
-    
+
+    $(document).on('click','.switch',function(i,o) {
+
+    	let ttt = array[Math.floor(Math.random()*array.length)];
+
+    	console.log(ttt);
+    	$('.num-'+ ttt).addClass('highlight');
+    	$('.switch_num').text(ttt);
+    })
+
 });
