84f5fdbfa7cbf0716d132ed8b88d1205974ea2a4
kazuto sato <private10969@yahoo.co.jp>
2022-11-30 16:57:18 +0900

    no message

--- andGirl/js/script.js
+++ andGirl/js/script.js
@@ -2,6 +2,14 @@ $(function(){
     $('button').on('click',function(){
         $('.side-menu').toggleClass('active');
     });
+
+/*    if (side-menu.hasClass('active')){
+        $('body').on('click',function(){
+            $('side-menu').removeClass('active');
+
+        })
+    }
+*/
     
 
 });
