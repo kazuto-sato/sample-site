de6d4ba8faee8809bb5b91ed837876e35739f48e
kazuto sato <private10969@yahoo.co.jp>
2022-11-30 19:02:44 +0900

    no message

--- andGirl/js/script.js
+++ andGirl/js/script.js
@@ -1,21 +1,18 @@
 $(function(){
+    //pc版のjsここから
+    $('.hover-list').on('click',function(){
+        console.log('fagfjs');//コンソールはされる
+        $('.hover-list-content').sildeToggle();
+    });
+    
+    //モバイル版のjsここから
     $('button').on('click',function(){
         $('.side-menu').toggleClass('active');
     });
-    $('.hover-list').on('click',function(){
-        $('.hover-list-content').slidToggle();
+
+    $('#js-main').on('click',function(){
+        if ($('.side-menu').hasClass('active')){
+            $('.side-menu').removeClass('active');
+        }
     });
-
-
-    /*
-   if ('side-menu'.hasClass('active')){
-        $('js-main').on('click',function(){
-            console.log('waaaaaaa');
-           // $('side-menu').removeClass('active');
-
-        });
-    }
-    */
-    
-
 });
